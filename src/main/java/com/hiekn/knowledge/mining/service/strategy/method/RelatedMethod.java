512ade0c96c5d99b891fe88328edae0ced869068
SYQlaile <792988601@QQ.com>
2019-03-27 16:46:06 +0800

    调整了一下重要性评价中double运算的精度问题.

--- src/main/java/com/hiekn/knowledge/mining/service/strategy/method/RelatedMethod.java
+++ src/main/java/com/hiekn/knowledge/mining/service/strategy/method/RelatedMethod.java
@@ -10,6 +10,8 @@ import com.hiekn.knowledge.mining.service.strategy.args.argsnull.ArgsNullEnum;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -59,7 +61,9 @@ public enum RelatedMethod implements MethodStrategy {
                 //用于接收
                 Map<String , Object> map = (Map)input;
                 //获取method类型
-                String method =  (String) map.get("method");
+                String method = (String) map.get("method");
+                //取小数点后面8位，四舍五入
+                MathContext mc = new MathContext(8, RoundingMode.HALF_DOWN);
                 //进行判断
                 if ("JOURNAL".equals(method)){
                     //加入四个key，用于在迭代的时候添加归一之后的数据
@@ -67,22 +71,33 @@ public enum RelatedMethod implements MethodStrategy {
                     map.put("搜索指数(归一)",null);
                     map.put("发文量(归一)",null);
                     map.put("影响因子(归一)",null);
-                    //遍历key
+//                    map.put("被引量(归一)",(Double.parseDouble(((String) map.get("被引量")))/1000000));
+//                    map.put("搜索指数(归一)",(Double.parseDouble(((String) map.get("搜索指数")))/100000));
+//                    map.put("发文量(归一)",(Double.parseDouble(((String) map.get("发文量")))/100000));
+//                    遍历key
                     for (String key : map.keySet()){
                         //进行判断
                         if ("被引量".equals(key)){
                             //对数据归一
-                            Double value = (Double.parseDouble(((String) map.get(key)))/1000000);
+                            //Double value= Math.atan(Double.parseDouble(((String) map.get(key))))*(2/Math.PI);
+                            BigDecimal bigDecimal = (BigDecimal.valueOf(Double.parseDouble(((String) map.get(key)))));
+                            BigDecimal value = bigDecimal.divide(BigDecimal.valueOf(1297080),mc);
                             map.put("被引量(归一)",value);
                         } else if ("搜索指数".equals(key)){
-                            Double value = (Double.parseDouble(((String) map.get(key)))/100000);
+                            //Double value= Math.atan(Double.parseDouble(((String) map.get(key))))*(2/Math.PI);
+                            BigDecimal bigDecimal = (BigDecimal.valueOf(Double.parseDouble(((String) map.get(key)))));
+                            BigDecimal value = bigDecimal.divide(BigDecimal.valueOf(180901),mc);
                             map.put("搜索指数(归一)",value);
                         } else if ("发文量".equals(key)) {
-                            Double value = (Double.parseDouble(((String) map.get(key)))/100000);
+                            //Double value= Math.atan(Double.parseDouble(((String) map.get(key))))*(2/Math.PI);
+                            BigDecimal bigDecimal = (BigDecimal.valueOf(Double.parseDouble(((String) map.get(key)))));
+                            BigDecimal value = bigDecimal.divide(BigDecimal.valueOf(108091),mc);
                             map.put("发文量(归一)",value);
                         }
                         else if ("影响因子".equals(key)){
-                            Double value = (Double.parseDouble(((String) map.get(key)))/10);
+                            //Double value= Math.atan(Double.parseDouble(((String) map.get(key))))*(2/Math.PI);
+                            BigDecimal bigDecimal = (BigDecimal.valueOf(Double.parseDouble(((String) map.get(key)))));
+                            BigDecimal value = bigDecimal.divide(BigDecimal.valueOf(11),mc);
                             map.put("影响因子(归一)",value);
                         }
                     }
@@ -112,22 +127,30 @@ public enum RelatedMethod implements MethodStrategy {
 //                            v2*args.getSearchindexweight()+
 //                            v3*args.getIdentificatedweight()+
 //                            v4*args.getFactorofinfluenceweight())/4;
-                    //加权平均
-                    double num =  (((double)map.get("被引量(归一)"))*v1+
-                            ((double)map.get("搜索指数(归一)"))*v2+
-                            ((double)map.get("发文量(归一)"))*v3+
-                            ((double)map.get("影响因子(归一)"))*v4)/4;
+//                            加权平均
+                    BigDecimal num =  ((
+                            (BigDecimal) map.get("被引量(归一)")).multiply(BigDecimal.valueOf(v1)).add((
+                                    (BigDecimal) map.get("搜索指数(归一)")).multiply(BigDecimal.valueOf(v2))) .add((
+                                    (BigDecimal) map.get("发文量(归一)")).multiply(BigDecimal.valueOf(v3))).add((
+                                    (BigDecimal) map.get("影响因子(归一)")).multiply(BigDecimal.valueOf(v4)))).divide((
+                            BigDecimal.valueOf(4)),mc);
                     //放入集合
                     map.put("加权平均值",num);
                 }else if ("LITERATURE".equals(method)){
+//                    map.put("被引量(归一)",(Double.parseDouble(((String) map.get("被引量：")))/1000000));
+//                    map.put("阅读量(归一)",(Double.parseDouble(((String) map.get("阅读量：")))/100000));
                     map.put("被引量(归一)",null);
                     map.put("阅读量(归一)",null);
                     for (String key : map.keySet()){
                         if ("被引量：".equals(key)) {
-                            Double value = (Double.parseDouble(((String) map.get(key)))/1000000);
+                            //double value = (Double.parseDouble(((String) map.get(key)))/1000000);
+                            BigDecimal bigDecimal = BigDecimal.valueOf(Double.parseDouble(((String) map.get(key))));
+                            BigDecimal value =  bigDecimal.divide(BigDecimal.valueOf(1701020),mc);
                             map.put("被引量(归一)",value);
                         }else if ("阅读量：".equals(key)){
-                            Double value = (Double.parseDouble(((String) map.get(key)))/100000);
+                            //double value = (Double.parseDouble(((String) map.get(key)))/100000);
+                            BigDecimal bigDecimal = BigDecimal.valueOf(Double.parseDouble(((String) map.get(key))));
+                            BigDecimal value =  bigDecimal.divide(BigDecimal.valueOf(1001),mc);
                             map.put("阅读量(归一)",value);
                         }
                     }
@@ -139,8 +162,12 @@ public enum RelatedMethod implements MethodStrategy {
                     if (v2==null){
                         v2=1.00;
                     }
-                    double num =  (((double)map.get("被引量(归一)"))*v1+
-                            ((double)map.get("阅读量(归一)"))*v2)/2;
+                    //加权平均
+                    BigDecimal num =  ((
+                            (BigDecimal) map.get("被引量(归一)")).multiply(BigDecimal.valueOf(v1)).add((
+                            (BigDecimal) map.get("阅读量(归一)")).multiply(BigDecimal.valueOf(v2)))
+                            .divide((
+                            BigDecimal.valueOf(2)),mc));
                     map.put("加权平均值",num);
                 }
                 mapReturn.put("result", map);
